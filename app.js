71d299b38b74bc2c592d1ad7c7816297950200ca
Mairie 13e - BV <dimitrilenet372@gmail.com>
2026-03-29 05:11:56 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -384,8 +384,8 @@ function resolveExoImg(exo) {
 // ══════════════════════════════════════════════
 // Remplace localStorage — tout est synchronisé en temps réel
 
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyBdb3K3X-sysepTYeVUFXhoQrgwl7VWpIA",
   authDomain: "zeroweigth.firebaseapp.com",
